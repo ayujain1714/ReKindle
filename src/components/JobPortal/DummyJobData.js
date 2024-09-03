4aff7040576d8a7dd14c317895ca65244a30ead2
Divyanshu Shekhar <149070310+Divyanshu832@users.noreply.github.com>
2024-09-03 22:14:33 +0530

    Add files via upload

---
+++ src/components/JobPortal/DummyJobData.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        id: 1,
+        postedOn : '2024-08-28',
+        title: 'IOS Developer',
+        company: 'Apple',
+        type: 'Full-Time',
+        experience: 'Fresher',
+        location: 'Remote',
+        skills: ["SwiftUI", "UIKit", "Core Data"],
+        job_link: "https://www.apple.com/careers/in/"
+    },
+    {
+        id: 2,
+        postedOn : '2024-08-25',
+        title: 'Full Stack Developer',
+        company: 'Microsoft',
+        type: 'Full-Time',
+        experience: '1-3 years',
+        location: 'In Office',
+        skills: ["MERN", "SQL", "AWS"],
+        job_link: "https://www.apple.com/careers/in/"
+    },
+    {
+        id: 3,
+        postedOn : '2024-08-23',
+        title: 'Frontend Developer',
+        company: 'Accenture',
+        type: 'Internship',
+        experience: 'Fresher',
+        location: 'In Office',
+        skills: ["React", "NextJS", "Javascript"],
+        job_link: "https://www.apple.com/careers/in/"
+    },
+    {
+        id: 4,
+        postedOn : '2024-08-20',
+        title: 'Blockchain Developer',
+        company: 'LangChain',
+        type: 'Full Time',
+        experience: '3+ years',
+        location: 'Remote',
+        skills: ["Solidity", "Smart Contracts", "BTC"],
+        job_link: "https://www.apple.com/careers/in/"
+    },
+    {
+        id: 5,
+        postedOn : '2024-09-01',
+        title: 'Backend Developer',
+        company: 'ZScaler',
+        type: 'Contract',
+        experience: '1-3 years',
+        location: 'Remote',
+        skills: ["Django", "NodeJS", "ExpressJS"],
+        job_link: "https://www.apple.com/careers/in/"
+    }
+]
